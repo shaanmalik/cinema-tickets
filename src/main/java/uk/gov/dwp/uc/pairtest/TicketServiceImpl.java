89d182e5b1891e027aca88488f770dd37eb22469
Shaan Malik <shaanmalik@gmail.com>
2022-12-20 17:29:19 +0000

    Initial commit

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -0,0 +1,85 @@
+package uk.gov.dwp.uc.pairtest;
+
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TicketServiceImpl implements TicketService {
+
+    private static final int MAX_TICKETS = 20;
+
+    private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
+
+    private final Map<TicketTypeRequest.Type, Integer> ticketTypeToPriceMap;
+
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService, SeatReservationService seatReservationService) {
+        this.ticketPaymentService = ticketPaymentService;
+        this.seatReservationService = seatReservationService;
+        ticketTypeToPriceMap = new HashMap<>();
+        ticketTypeToPriceMap.put(TicketTypeRequest.Type.ADULT, 20);
+        ticketTypeToPriceMap.put(TicketTypeRequest.Type.CHILD, 10);
+    }
+
+
+    /**
+     * Should only have private methods other than the one below.
+     */
+    @Override
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+
+        if (!(accountId > 0)) {
+            // only accountIds greater than 0 are valid
+            throw new InvalidPurchaseException();
+        }
+
+        int numTicketsPurchased = 0;
+        int totalSeats = 0;
+
+        int totalAmountToPay = 0;
+
+        int totalAdultTickets = 0;
+        int totalChildTickets = 0;
+        int totalInfantTickets = 0;
+
+        for (TicketTypeRequest ticketTypeRequest : ticketTypeRequests) {
+
+            numTicketsPurchased += ticketTypeRequest.getNoOfTickets();
+
+            switch (ticketTypeRequest.getTicketType()) {
+                case ADULT:
+                    totalAdultTickets += ticketTypeRequest.getNoOfTickets();
+                    break;
+                case CHILD:
+                    totalChildTickets += ticketTypeRequest.getNoOfTickets();
+                    break;
+                case INFANT:
+                    totalInfantTickets += ticketTypeRequest.getNoOfTickets();
+                    break;
+            }
+
+            if (totalChildTickets + totalInfantTickets > 0 && totalAdultTickets == 0) {
+                // cannot purchase child or infant tickets without at least 1 adult ticket
+                throw new InvalidPurchaseException();
+            }
+
+
+            if (numTicketsPurchased > MAX_TICKETS) {
+                throw new InvalidPurchaseException();
+            }
+
+            if (!ticketTypeRequest.getTicketType().equals(TicketTypeRequest.Type.INFANT)) {
+                totalSeats += ticketTypeRequest.getNoOfTickets();
+                totalAmountToPay += ticketTypeRequest.getNoOfTickets() * ticketTypeToPriceMap.get(ticketTypeRequest.getTicketType());
+            }
+        }
+
+        seatReservationService.reserveSeat(accountId, totalSeats);
+        ticketPaymentService.makePayment(accountId, totalAmountToPay);
+    }
+}
---
+++ src/test/java/TicketServiceTest.java
@@ -0,0 +1,139 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TicketServiceTest {
+
+    @Mock
+    TicketPaymentService ticketPaymentService;
+
+    @Mock
+    SeatReservationService seatReservationService;
+
+    @InjectMocks
+    TicketServiceImpl ticketService;
+
+    private static final long ACCOUNT_ID = 1L;
+
+    @Test
+    public void testPurchase1AdultTicket() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+
+        Mockito.verify(ticketPaymentService).makePayment(ACCOUNT_ID, 20);
+        Mockito.verify(seatReservationService).reserveSeat(ACCOUNT_ID, 1);
+
+    }
+
+    @Test
+    public void testPurchase1Adult1ChildTicket() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1)
+                );
+
+        Mockito.verify(ticketPaymentService).makePayment(ACCOUNT_ID, 30);
+        Mockito.verify(seatReservationService).reserveSeat(ACCOUNT_ID, 2);
+
+    }
+
+    @Test
+    public void testPurchase1Adult1Child1InfantTicket() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1)
+        );
+
+        Mockito.verify(ticketPaymentService).makePayment(ACCOUNT_ID, 30);
+        Mockito.verify(seatReservationService).reserveSeat(ACCOUNT_ID, 2);
+
+    }
+
+    @Test
+    public void testPurchase2Adult2Child1InfantTicket() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2),
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1)
+        );
+
+        Mockito.verify(ticketPaymentService).makePayment(ACCOUNT_ID, 60);
+        Mockito.verify(seatReservationService).reserveSeat(ACCOUNT_ID, 4);
+
+    }
+
+
+    @Test
+    public void testPurchase1Adult1Adult1Child1Child1InfantTicket() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1)
+        );
+
+        Mockito.verify(ticketPaymentService).makePayment(ACCOUNT_ID, 60);
+        Mockito.verify(seatReservationService).reserveSeat(ACCOUNT_ID, 4);
+
+    }
+
+    @Test
+    public void testPurchaseMixedTickets() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2),
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 3),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2),
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 2)
+        );
+
+        Mockito.verify(ticketPaymentService).makePayment(ACCOUNT_ID, 110);
+        Mockito.verify(seatReservationService).reserveSeat(ACCOUNT_ID, 8);
+
+    }
+
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testPurchase1InfantTicketInvalid() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1)
+        );
+
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testPurchase1Infant1ChildTicketInvalid() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1)
+        );
+
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testPurchase1ChildTicketInvalid() {
+
+        ticketService.purchaseTickets(ACCOUNT_ID,
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1)
+        );
+
+    }
+}
